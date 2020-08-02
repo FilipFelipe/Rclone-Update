8feb41021b28ce2e6d72a4d1bbef7f5312009995
Filip Felipe <filip_junio@hotmail.com>
2020-08-02 17:23:20 -0300

    Novas Funções
    * Carregar as nuvens cadastradas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,7 +32,6 @@
             this.Upload = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.folder_nuvem_upload = new System.Windows.Forms.TextBox();
             this.folderupload = new System.Windows.Forms.TextBox();
             this.sync_upload = new System.Windows.Forms.Button();
             this.upload_button = new System.Windows.Forms.Button();
@@ -40,23 +39,44 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.folderdownload = new System.Windows.Forms.TextBox();
-            this.folder_nuvem_download = new System.Windows.Forms.TextBox();
             this.sync_download = new System.Windows.Forms.Button();
             this.downalod_button = new System.Windows.Forms.Button();
             this.config = new System.Windows.Forms.Button();
+            this.boxname = new System.Windows.Forms.ComboBox();
+            this.carregar = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.sairToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ferramentasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personalizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.conteúdoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.sobreToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.folder_nuvem_upload = new System.Windows.Forms.ComboBox();
+            this.folder_nuvem_download = new System.Windows.Forms.ComboBox();
+            this.limparCacheToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Nome = new System.Windows.Forms.Label();
             this.Upload.SuspendLayout();
             this.Download.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Upload
             // 
+            this.Upload.Controls.Add(this.folder_nuvem_upload);
             this.Upload.Controls.Add(this.label2);
             this.Upload.Controls.Add(this.label1);
-            this.Upload.Controls.Add(this.folder_nuvem_upload);
             this.Upload.Controls.Add(this.folderupload);
             this.Upload.Controls.Add(this.sync_upload);
             this.Upload.Controls.Add(this.upload_button);
-            this.Upload.Location = new System.Drawing.Point(12, 16);
+            this.Upload.Location = new System.Drawing.Point(12, 111);
             this.Upload.Name = "Upload";
             this.Upload.Size = new System.Drawing.Size(388, 179);
             this.Upload.TabIndex = 0;
@@ -80,13 +100,6 @@
             this.label1.Size = new System.Drawing.Size(129, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Diretório de sincronização";
-            // 
-            // folder_nuvem_upload
-            // 
-            this.folder_nuvem_upload.Location = new System.Drawing.Point(6, 93);
-            this.folder_nuvem_upload.Name = "folder_nuvem_upload";
-            this.folder_nuvem_upload.Size = new System.Drawing.Size(241, 20);
-            this.folder_nuvem_upload.TabIndex = 3;
             // 
             // folderupload
             // 
@@ -121,13 +134,13 @@
             // 
             // Download
             // 
+            this.Download.Controls.Add(this.folder_nuvem_download);
             this.Download.Controls.Add(this.label4);
             this.Download.Controls.Add(this.label3);
             this.Download.Controls.Add(this.folderdownload);
-            this.Download.Controls.Add(this.folder_nuvem_download);
             this.Download.Controls.Add(this.sync_download);
             this.Download.Controls.Add(this.downalod_button);
-            this.Download.Location = new System.Drawing.Point(406, 16);
+            this.Download.Location = new System.Drawing.Point(406, 111);
             this.Download.Name = "Download";
             this.Download.Size = new System.Drawing.Size(388, 179);
             this.Download.TabIndex = 1;
@@ -139,9 +152,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(6, 77);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(129, 13);
+            this.label4.Size = new System.Drawing.Size(98, 13);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Diretório de sincronização";
+            this.label4.Text = "Diretório na Nuvem";
             // 
             // label3
             // 
@@ -158,13 +171,6 @@
             this.folderdownload.Name = "folderdownload";
             this.folderdownload.Size = new System.Drawing.Size(341, 20);
             this.folderdownload.TabIndex = 3;
-            // 
-            // folder_nuvem_download
-            // 
-            this.folder_nuvem_download.Location = new System.Drawing.Point(6, 93);
-            this.folder_nuvem_download.Name = "folder_nuvem_download";
-            this.folder_nuvem_download.Size = new System.Drawing.Size(241, 20);
-            this.folder_nuvem_download.TabIndex = 2;
             // 
             // sync_download
             // 
@@ -191,13 +197,177 @@
             // 
             // config
             // 
-            this.config.Location = new System.Drawing.Point(12, 208);
+            this.config.Location = new System.Drawing.Point(49, 33);
             this.config.Name = "config";
-            this.config.Size = new System.Drawing.Size(136, 30);
+            this.config.Size = new System.Drawing.Size(117, 21);
             this.config.TabIndex = 4;
             this.config.Text = "Configuração Rclone";
             this.config.UseVisualStyleBackColor = true;
             this.config.Click += new System.EventHandler(this.config_Click);
+            // 
+            // boxname
+            // 
+            this.boxname.FormattingEnabled = true;
+            this.boxname.Location = new System.Drawing.Point(182, 34);
+            this.boxname.Name = "boxname";
+            this.boxname.Size = new System.Drawing.Size(99, 21);
+            this.boxname.TabIndex = 5;
+            this.boxname.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // carregar
+            // 
+            this.carregar.Location = new System.Drawing.Point(287, 33);
+            this.carregar.Name = "carregar";
+            this.carregar.Size = new System.Drawing.Size(99, 21);
+            this.carregar.TabIndex = 6;
+            this.carregar.Text = "Atualizar Nuvem";
+            this.carregar.UseVisualStyleBackColor = true;
+            this.carregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.ferramentasToolStripMenuItem,
+            this.ajudaToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(801, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.toolStripSeparator,
+            this.sairToolStripMenuItem1});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "&Arquivo";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("novoToolStripMenuItem.Image")));
+            this.novoToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.novoToolStripMenuItem.Text = "&Novo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("abrirToolStripMenuItem.Image")));
+            this.abrirToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "&Abrir";
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(177, 6);
+            // 
+            // sairToolStripMenuItem1
+            // 
+            this.sairToolStripMenuItem1.Name = "sairToolStripMenuItem1";
+            this.sairToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem1.Text = "Sai&r";
+            // 
+            // ferramentasToolStripMenuItem
+            // 
+            this.ferramentasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.personalizarToolStripMenuItem,
+            this.opçõesToolStripMenuItem,
+            this.limparCacheToolStripMenuItem});
+            this.ferramentasToolStripMenuItem.Name = "ferramentasToolStripMenuItem";
+            this.ferramentasToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.ferramentasToolStripMenuItem.Text = "Ferra&mentas";
+            // 
+            // personalizarToolStripMenuItem
+            // 
+            this.personalizarToolStripMenuItem.Name = "personalizarToolStripMenuItem";
+            this.personalizarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.personalizarToolStripMenuItem.Text = "&Personalizar";
+            // 
+            // opçõesToolStripMenuItem
+            // 
+            this.opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+            this.opçõesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.opçõesToolStripMenuItem.Text = "&Opções";
+            // 
+            // ajudaToolStripMenuItem1
+            // 
+            this.ajudaToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.conteúdoToolStripMenuItem,
+            this.toolStripSeparator5,
+            this.sobreToolStripMenuItem1});
+            this.ajudaToolStripMenuItem1.Name = "ajudaToolStripMenuItem1";
+            this.ajudaToolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.ajudaToolStripMenuItem1.Text = "Aj&uda";
+            // 
+            // conteúdoToolStripMenuItem
+            // 
+            this.conteúdoToolStripMenuItem.Name = "conteúdoToolStripMenuItem";
+            this.conteúdoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.conteúdoToolStripMenuItem.Text = "&Conteúdo";
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(177, 6);
+            // 
+            // sobreToolStripMenuItem1
+            // 
+            this.sobreToolStripMenuItem1.Name = "sobreToolStripMenuItem1";
+            this.sobreToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.sobreToolStripMenuItem1.Text = "&Sobre...";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.carregar);
+            this.groupBox1.Controls.Add(this.boxname);
+            this.groupBox1.Controls.Add(this.config);
+            this.groupBox1.Location = new System.Drawing.Point(12, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(789, 73);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuração";
+            // 
+            // folder_nuvem_upload
+            // 
+            this.folder_nuvem_upload.FormattingEnabled = true;
+            this.folder_nuvem_upload.Location = new System.Drawing.Point(6, 92);
+            this.folder_nuvem_upload.Name = "folder_nuvem_upload";
+            this.folder_nuvem_upload.Size = new System.Drawing.Size(267, 21);
+            this.folder_nuvem_upload.TabIndex = 7;
+            // 
+            // folder_nuvem_download
+            // 
+            this.folder_nuvem_download.FormattingEnabled = true;
+            this.folder_nuvem_download.Location = new System.Drawing.Point(6, 93);
+            this.folder_nuvem_download.Name = "folder_nuvem_download";
+            this.folder_nuvem_download.Size = new System.Drawing.Size(267, 21);
+            this.folder_nuvem_download.TabIndex = 9;
+            // 
+            // limparCacheToolStripMenuItem
+            // 
+            this.limparCacheToolStripMenuItem.Name = "limparCacheToolStripMenuItem";
+            this.limparCacheToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.limparCacheToolStripMenuItem.Text = "Limpar Cache";
+            this.limparCacheToolStripMenuItem.Click += new System.EventHandler(this.limparCacheToolStripMenuItem_Click);
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(775, 293);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(19, 13);
+            this.Nome.TabIndex = 9;
+            this.Nome.Text = "FF";
             // 
             // Main
             // 
@@ -205,20 +375,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(805, 241);
+            this.ClientSize = new System.Drawing.Size(801, 306);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Download);
             this.Controls.Add(this.Upload);
-            this.Controls.Add(this.config);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Rclone Update Files   V0.1 - Filip Junio Felipe";
+            this.Text = "Rclone Update Files   V0.15 - Filip Junio Felipe";
             this.Upload.ResumeLayout(false);
             this.Upload.PerformLayout();
             this.Download.ResumeLayout(false);
             this.Download.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -228,17 +404,35 @@
         private System.Windows.Forms.GroupBox Download;
         private System.Windows.Forms.Button upload_button;
         private System.Windows.Forms.Button downalod_button;
-        private System.Windows.Forms.TextBox folder_nuvem_upload;
         private System.Windows.Forms.TextBox folderupload;
         private System.Windows.Forms.Button sync_upload;
         private System.Windows.Forms.TextBox folderdownload;
-        private System.Windows.Forms.TextBox folder_nuvem_download;
         private System.Windows.Forms.Button sync_download;
         private System.Windows.Forms.Button config;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox boxname;
+        private System.Windows.Forms.Button carregar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ferramentasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personalizarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem conteúdoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox folder_nuvem_upload;
+        private System.Windows.Forms.ComboBox folder_nuvem_download;
+        private System.Windows.Forms.ToolStripMenuItem limparCacheToolStripMenuItem;
+        private System.Windows.Forms.Label Nome;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
+
 namespace Rclone_Update
 {
     public partial class Main : Form
@@ -15,8 +17,29 @@ namespace Rclone_Update
         public Main()
         {
             InitializeComponent();
+            iniciar_variaveis();
         }
+        void iniciar_variaveis()
+        {
+            //nuvem
+            string[] text = System.IO.File.ReadAllLines(@"rclone\config\.nuvem.dat");
+            foreach (string texto in text)
+            {
 
+                Console.WriteLine("\t" + texto);
+                boxname.Items.Add(texto);
+            }
+            
+            //nuvem
+            string[] upload = System.IO.File.ReadAllLines(@"rclone\config\.upload.dat");
+            foreach (string texto in upload)
+            {
+
+                Console.WriteLine("\t" + texto);
+                folder_nuvem_upload.Items.Add(texto);
+            }
+           
+        }
 
 
 
@@ -34,13 +57,13 @@ namespace Rclone_Update
             }
             else
             {
-                
+
             }
         }
 
         private void downalodbutton_Click(object sender, EventArgs e)
         {
-      
+
             FolderBrowserDialog downalod_folder = new FolderBrowserDialog();
             downalod_folder.RootFolder = Environment.SpecialFolder.Desktop;
             downalod_folder.Description = "Selecione a pasta para sinconizacao";
@@ -51,26 +74,37 @@ namespace Rclone_Update
             }
             else
             {
-                
+
             }
         }
 
         private void sync_upload_Click(object sender, EventArgs e)
         {
-            
+
             ProcessStartInfo startInfo = new ProcessStartInfo("rclone\\rclone.exe");
-            startInfo.Arguments = "sync \"" + folderupload.Text + "\" " + "onedrive:\"" + folder_nuvem_upload.Text + "\" -P";
-           
+            startInfo.Arguments = "sync \"" + folderupload.Text + "\" " + boxname.Text + "\"" + folder_nuvem_upload.Text + "\" -P";
             Process.Start(startInfo);
+            using (StreamWriter writer = new StreamWriter(@"rclone\config\.upload.dat", true))
+            {
+                writer.WriteLine(folder_nuvem_upload.Text);
+            }
+            folder_nuvem_upload.Items.Add(folder_nuvem_upload.Text);
+            folder_nuvem_upload.Text = "";
         }
 
         private void sync_download_Click(object sender, EventArgs e)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo("rclone\\rclone.exe");
-            startInfo.Arguments = "sync onedrive:\"" + folder_nuvem_download.Text + "\"" + " \"" + folderdownload.Text + "\\" + folder_nuvem_download.Text + "\" -P";
+            startInfo.Arguments = "sync " + boxname.Text + "\"" + folder_nuvem_download.Text + "\"" + " \"" + folderdownload.Text + "\\" + folder_nuvem_download.Text + "\" -P";
             Process.Start(startInfo);
+            using (StreamWriter writer = new StreamWriter(@"rclone\config\.download.dat", true))
+            {
+                writer.WriteLine(folder_nuvem_download.Text);
+            }
+            folder_nuvem_download.Items.Add(folder_nuvem_download.Text);
+            folder_nuvem_download.Text = "";
         }
-            
+
         private void folderupload_TextChanged(object sender, EventArgs e)
         {
 
@@ -81,9 +115,65 @@ namespace Rclone_Update
         private void config_Click(object sender, EventArgs e)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo("rclone\\rclone.exe");
-            startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            //startInfo.WindowStyle = ProcessWindowStyle.Minimized;
             startInfo.Arguments = "config";
             Process.Start(startInfo);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            boxname.Items.Clear();
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "rclone\\rclone.exe";
+                process.StartInfo.Arguments = "listremotes";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                process.Start();
+                StreamReader reader = process.StandardOutput;
+                string output = reader.ReadToEnd();
+                using (StreamWriter writer = new StreamWriter(@"rclone\config\.nuvem.dat", false))
+                {
+                    writer.WriteLine(output);
+                }
+                process.WaitForExit();
+            }
+            string[] text = System.IO.File.ReadAllLines(@"rclone\config\.nuvem.dat");
+            foreach (string texto in text)
+            {
+
+                Console.WriteLine("\t" + texto);
+                boxname.Items.Add(texto);
+            }
+            boxname.SelectedIndex = 0;
+        }
+
+        private void limparCacheToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter(@"rclone\config\.nuvem.dat", false))
+            {
+                writer.Close();
+            }
+            using (StreamWriter writer = new StreamWriter(@"rclone\config\.download.dat", false))
+            {
+                writer.Close();
+            }
+            using (StreamWriter writer = new StreamWriter(@"rclone\config\.upload.dat", false))
+            {
+                writer.Close();
+            }
+            boxname.Items.Clear();
+            folder_nuvem_download.Items.Clear();
+            folder_nuvem_upload.Items.Clear();
+            boxname.Text = "";
+            folder_nuvem_download.Text = "";
+            folder_nuvem_upload.Text = "";
         }
     }
 }
